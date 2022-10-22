4cf9efac422683f587909a67816d1b2a25c1e318
kooyunhyung <kooyh108@naver.com>
2022-10-22 19:05:05 +0900

    GROAD: 지도 API 활용 + 코스 임시 구현

--- lib/page/Main_screens/TravelCourse/TravelCourse.dart
+++ lib/page/Main_screens/TravelCourse/TravelCourse.dart
@@ -1,29 +1,109 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gload_app/constant/theme.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 import '../../frame/common_frame2.dart';
 import 'CultureMore/Budda/BuddaInfo.dart';
 import 'CultureMore/Confucianism/Confucianism_Info.dart';
 import 'PositionPage/Position_YetSanGil/PositionPage_YetSanGil.dart';
 
-class TravelCourse extends StatelessWidget {
+class TravelCourse extends StatefulWidget {
   const TravelCourse({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<TravelCourse> createState() => _TravelCourseState();
+}
+
+class _TravelCourseState extends State<TravelCourse> {
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+  CameraPosition _cameraPosition =
+      CameraPosition(target: LatLng(37.8751681, 127.7444707), zoom: 11.7);
+
+  _TravelCourseState();
+
+  @override
+  build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
+    Marker marker1 = Marker(
+      markerId: 'Chuncheon Station',
+      position: LatLng(37.8844239, 127.7165788),
+      captionText: "춘천역",
+      captionColor: Colors.red,
+      captionTextSize: 15,
+      width: 20,
+      height: 30,
+    );
+    Marker marker2 = Marker(
+      markerId: 'Paradise Culture Park',
+      position: LatLng(37.8778473, 127.7232115),
+      captionText: "낙원 문화 공원",
+      captionColor: Colors.red,
+      captionTextSize: 13,
+      width: 20,
+      height: 30,
+    );
+    Marker marker3 = Marker(
+        markerId: 'Juknim-dong Cathedral',
+        position: LatLng(37.876364, 127.7270891),
+        captionText: "죽림동 성당",
+        captionOffset: 10,
+        captionColor: Colors.red,
+        captionTextSize: 13,
+        width: 20,
+        height: 30);
+    Marker marker4 = Marker(
+        markerId: 'Chuncheon Encounter Forest',
+        position: LatLng(37.8685332, 127.7227753),
+        captionText: "춘천 만남의 숲",
+        captionColor: Colors.red,
+        captionTextSize: 13,
+        width: 20,
+        height: 30);
+    Marker marker5 = Marker(
+        markerId: 'Jiseong Park',
+        position: LatLng(37.8679131, 127.7368356),
+        captionText: "지성 공원",
+        captionColor: Colors.red,
+        captionTextSize: 14,
+        width: 20,
+        height: 30);
+    Marker marker6 = Marker(
+        markerId: 'Chuncheon National University of Education',
+        position: LatLng(37.8590126, 127.7483479),
+        captionText: "춘천 교육 대학교",
+        captionColor: Colors.red,
+        captionTextSize: 15,
+        onMarkerTab: _onMarkerTap6,
+        width: 20,
+        height: 30);
+    final pathOverlayId = PathOverlayId("course1");
+    List<LatLng> path = [
+      LatLng(37.8844239, 127.7165788),
+      LatLng(37.8778473, 127.7232115),
+      LatLng(37.876364, 127.7270891),
+      LatLng(37.8685332, 127.7227753),
+      LatLng(37.8679131, 127.7368356),
+      LatLng(37.8590126, 127.7483479)
+    ];
+    final Set<PathOverlay> _pathOverlays = new Set<PathOverlay>();
+    _pathOverlays
+        .add(PathOverlay(pathOverlayId, path, width: 5, outlineWidth: 1));
 
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Column(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(horizontal: width*0.037593, vertical: height*0.03759),
+            padding: EdgeInsets.symmetric(
+                horizontal: width * 0.037593, vertical: height * 0.03759),
             //color: Colors.grey[100],
             child: Column(children: [
               Padding(
-                padding: EdgeInsets.only(right: width*0.60827),
+                padding: EdgeInsets.only(right: width * 0.60827),
                 child: Column(
                   children: [
                     Text(
@@ -35,58 +115,67 @@ class TravelCourse extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      height: height*0.00877,
+                      height: height * 0.00877,
                     ),
                     Container(
                       color: Color(ThemeColors.deepNavy),
-                      width: width*0.243309,
+                      width: width * 0.243309,
                       height: 3.0,
                     ),
                   ],
                 ), //1번 코스
               ),
               SizedBox(
-                height: height*0.03132,
+                height: height * 0.03132,
               ),
               InkWell(
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => CommonFrame2(title: '해당 코스', clas: PositionPage_YetSanGil(),)),
+                    MaterialPageRoute(
+                        builder: (context) => CommonFrame2(
+                              title: '해당 코스',
+                              clas: PositionPage_YetSanGil(),
+                            )),
                   );
                 },
                 child: Center(
                   child: Container(
-                    width: width * 0.85,
-                    height: height*0.31328,
-                    color: Colors.grey[400],
-                    child: Center(
-                      child: Text(
-                        '해당 코스 지도',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
+                      width: width * 0.85,
+                      height: height * 0.31328,
+                      //color: Colors.grey[400],
+                      child: NaverMap(
+                        //onMapTap: _onMapTap,
+                        onMapCreated: onMapCreated,
+                        mapType: _mapType,
+                        initialCameraPosition: _cameraPosition,
+                        markers: [
+                          marker1,
+                          marker2,
+                          marker3,
+                          marker4,
+                          marker5,
+                          marker6
+                        ],
+                        pathOverlays: _pathOverlays,
+                      )),
                 ),
               ),
             ]),
           ), //해당 코스 지도
           SizedBox(
-            height: height*0.018796,
+            height: height * 0.018796,
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*0.048661),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.048661),
             child: Stack(
               children: [
                 Padding(
-                  padding: EdgeInsets.only(top: height*0.005012),
+                  padding: EdgeInsets.only(top: height * 0.005012),
                   child: Center(
                     child: Container(
                       color: Color(ThemeColors.lightSky),
-                      width: width*0.81508515,
+                      width: width * 0.81508515,
                       height: 1.0,
                     ),
                   ),
@@ -100,7 +189,7 @@ class TravelCourse extends StatelessWidget {
                       size: 10.0,
                     ),
                     SizedBox(
-                      width: width*0.04866,
+                      width: width * 0.04866,
                     ),
                     Icon(
                       Icons.circle,
@@ -108,7 +197,7 @@ class TravelCourse extends StatelessWidget {
                       size: 10.0,
                     ),
                     SizedBox(
-                      width: width*0.085158,
+                      width: width * 0.085158,
                     ),
                     Icon(
                       Icons.circle,
@@ -116,7 +205,7 @@ class TravelCourse extends StatelessWidget {
                       size: 10.0,
                     ),
                     SizedBox(
-                      width: width*0.12165,
+                      width: width * 0.12165,
                     ),
                     Icon(
                       Icons.circle,
@@ -124,7 +213,7 @@ class TravelCourse extends StatelessWidget {
                       size: 10.0,
                     ),
                     SizedBox(
-                      width: width*0.12165,
+                      width: width * 0.12165,
                     ),
                     Icon(
                       Icons.circle,
@@ -140,32 +229,37 @@ class TravelCourse extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Text(
-                "춘천역",
-                style: TextStyle(color: Color(ThemeColors.deepNavy), fontSize: 12.0),
+                "낙원 문화 공원",
+                style: TextStyle(
+                    color: Color(ThemeColors.deepNavy), fontSize: 12.0),
               ),
               Text(
-                "옛산길",
-                style: TextStyle(color: Color(ThemeColors.deepNavy), fontSize: 12.0),
+                "죽림동 성당",
+                style: TextStyle(
+                    color: Color(ThemeColors.deepNavy), fontSize: 12.0),
               ),
               Text(
-                "고개마루에서 직진",
-                style: TextStyle(color: Color(ThemeColors.deepNavy), fontSize: 12.0),
+                "춘천 만남의 숲",
+                style: TextStyle(
+                    color: Color(ThemeColors.deepNavy), fontSize: 12.0),
               ),
               Text(
-                "미나리폭포",
-                style: TextStyle(color: Color(ThemeColors.deepNavy), fontSize: 12.0),
+                "지성 공원",
+                style: TextStyle(
+                    color: Color(ThemeColors.deepNavy), fontSize: 12.0),
               ),
               Text(
-                "소양로 성당",
-                style: TextStyle(color: Color(ThemeColors.deepNavy), fontSize: 12.0),
+                "춘천 교육 대학교",
+                style: TextStyle(
+                    color: Color(ThemeColors.deepNavy), fontSize: 12.0),
               ),
             ],
           ), //춘천역 옛산길 고개마루에서 직진
           SizedBox(
-            height: height*0.06265,
+            height: height * 0.06265,
           ),
           Padding(
-            padding: EdgeInsets.only(left: width*0.048661),
+            padding: EdgeInsets.only(left: width * 0.048661),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
@@ -195,7 +289,7 @@ class TravelCourse extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*0.02433),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.02433),
             child: Text(
               '람 사는 냄새가 물씬 풍기는 번개 시장을 스쳐 지나면 해질녘 노',
               style: TextStyle(
@@ -204,7 +298,7 @@ class TravelCourse extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: width*0.04136),
+            padding: EdgeInsets.only(left: width * 0.04136),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -218,7 +312,7 @@ class TravelCourse extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(right: width*0.02433),
+            padding: EdgeInsets.only(right: width * 0.02433),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -234,10 +328,10 @@ class TravelCourse extends StatelessWidget {
             ),
           ), //글귀
           SizedBox(
-            height: height*0.03132,
+            height: height * 0.03132,
           ),
           Padding(
-            padding: EdgeInsets.only(left: width*0.04866),
+            padding: EdgeInsets.only(left: width * 0.04866),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -253,21 +347,25 @@ class TravelCourse extends StatelessWidget {
             ),
           ), //문화 더보기
           SizedBox(
-            height: height*0.025062,
+            height: height * 0.025062,
           ),
           InkWell(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => CommonFrame2(title:'불교문화', clas: BuddaInfoPage(),)),
+                MaterialPageRoute(
+                    builder: (context) => CommonFrame2(
+                          title: '불교문화',
+                          clas: BuddaInfoPage(),
+                        )),
               );
             },
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Container(
                 color: Colors.grey[400],
-                width: width*0.7274,
-                height: height*0.032581,
+                width: width * 0.7274,
+                height: height * 0.032581,
                 child: Center(
                   child: Text(
                     '불교문화',
@@ -281,20 +379,26 @@ class TravelCourse extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: height*0.00626,),
+          SizedBox(
+            height: height * 0.00626,
+          ),
           InkWell(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => CommonFrame2(title: '유교 문화', clas: Confucianism_InfoPage(),)),
+                MaterialPageRoute(
+                    builder: (context) => CommonFrame2(
+                          title: '유교 문화',
+                          clas: Confucianism_InfoPage(),
+                        )),
               );
             },
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Container(
                 color: Colors.grey[400],
-                width: width*0.7274,
-                height: height*0.032581,
+                width: width * 0.7274,
+                height: height * 0.032581,
                 child: Center(
                   child: Text(
                     '유교문화',
@@ -308,15 +412,17 @@ class TravelCourse extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: height*0.00626,),
+          SizedBox(
+            height: height * 0.00626,
+          ),
           InkWell(
             onTap: () {},
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Container(
                 color: Colors.grey[400],
-                width: width*0.7274,
-                height: height*0.032581,
+                width: width * 0.7274,
+                height: height * 0.032581,
                 child: Center(
                   child: Text(
                     '현대 콘텐츠',
@@ -331,10 +437,26 @@ class TravelCourse extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: height*0.03759,
+            height: height * 0.03759,
           )
         ],
       ),
     );
+  }
+
+  void _onMarkerTap6(Marker marker, Map<String, int> iconSize) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => CommonFrame2(
+                title: '불교문화',
+                clas: BuddaInfoPage(),
+              )),
+    );
+  }
+
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
   }
 }
