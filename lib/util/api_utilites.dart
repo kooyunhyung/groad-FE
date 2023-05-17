68c9db242fb0d7aea2e5622e6d73f2e54e068b3b
kooyunhyung <kooyh108@naver.com>
2023-05-17 18:20:41 +0900

    fix host

--- lib/util/api_utilites.dart
+++ lib/util/api_utilites.dart
@@ -7,7 +7,7 @@ class APIConfig {
   APIConfig(this.host, {this.version});
 }
 
-final host = APIConfig('http://3.36.5.43:8000',version: 1);
+final host = APIConfig('http://210.115.229.254:8000', version: 1);
 
 
 String getQueryString(Map params,
